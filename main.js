1cdca76787f6cda52ad555beeac33c5dd9ee4395
Fam <fam0075552@gmail.com>
2026-07-06 17:22:53 +0700

    9.5 commit

--- main.js
+++ main.js
@@ -1,69 +1,14 @@
 const { createApp, ref, computed } = Vue
 
-createApp ({
+const app = createApp({
     setup(){
-        const product = ref('Boots')
-        const brand = ref('SE 331')
-        // const image = ref('./assets/images/socks_green.jpg')
-        const link = ref('https://www.camt.cmu.ac.th')
-        // const inStock = ref(true)
-        const inventory = ref(100)
-        const onSale = ref(true)
-        const details = ref([
-            '50% cotton',
-            '30% wool',
-            '20% polyester'
-        ])
-        const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue ', image: './assets/images/socks_blue.jpg', quantity: 0 }
-        ])
-        const selectedVariant = ref(0)
         const cart = ref(0)
-        function addToCart(){
-            cart.value +=1
-        }
-        const title = computed(() =>{
-            return brand.value + ' ' + product.value
-        })
-        const image = computed(() => {
-            return variants.value[selectedVariant.value].image
-        })
-        const inStock = computed(() => {
-            return variants.value[selectedVariant.value].quantity
-        })
-        const onSaleMessage = computed(() => {
-            return brand.value + ' ' + product.value + ' is on sale'
-        })
-        function updateImage(variantImage){
-            image.value = variantImage
-        }
-        function inStockStatus(){
-            inStock.value = !inStock.value
-        }
-        function updateVariant(index){
-            selectedVariant.value = index;
-        }
-
-        const sizes = ref(['S', 'M', 'L'])
-
         return {
-            title,
-            image,
-            inStock,
-            onSaleMessage,
-            inventory,
-            details,
-            variants,
-            cart,
-            addToCart,
-            updateImage,
-            updateVariant,
-            sizes,
-            onSale,
-            link
+            cart
         }
     }
+})
 
+app.component('product-display', productDisplay)
 
-}).mount('#app')
+app.mount('#app')
