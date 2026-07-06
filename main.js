014181222b26d0b7603dcfb6ac8a7b0bfd01b3fe
Fam <fam0075552@gmail.com>
2026-07-06 14:16:49 +0700

    Adding onSale

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ createApp ({
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th')
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const onSale = ref(true)
         return {
             product,
             image,
+            inStock,
+            inventory,
+            onSale,
             link
         }
     }
