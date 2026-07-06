e6505fc21a936e07c3618d74b5d024eda844e9b0
Fam <fam0075552@gmail.com>
2026-07-06 16:51:09 +0700

    Adding onSaleMessage and updateVariant

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue
 
 createApp ({
     setup(){
         const product = ref('Boots')
-        const image = ref('./assets/images/socks_green.jpg')
+        const brand = ref('SE 331')
+        // const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th')
-        const inStock = ref(true)
+        // const inStock = ref(true)
         const inventory = ref(100)
         const onSale = ref(true)
         const details = ref([
@@ -14,32 +15,50 @@ createApp ({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue ', image: './assets/images/socks_blue.jpg' }
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue ', image: './assets/images/socks_blue.jpg', quantity: 0 }
         ])
+        const selectedVariant = ref(0)
         const cart = ref(0)
         function addToCart(){
             cart.value +=1
         }
+        const title = computed(() =>{
+            return brand.value + ' ' + product.value
+        })
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity
+        })
+        const onSaleMessage = computed(() => {
+            return brand.value + ' ' + product.value + ' is on sale'
+        })
         function updateImage(variantImage){
             image.value = variantImage
         }
         function inStockStatus(){
             inStock.value = !inStock.value
         }
+        function updateVariant(index){
+            selectedVariant.value = index;
+        }
+
         const sizes = ref(['S', 'M', 'L'])
 
         return {
-            product,
+            title,
             image,
             inStock,
+            onSaleMessage,
             inventory,
             details,
             variants,
             cart,
             addToCart,
             updateImage,
-            inStockStatus,
+            updateVariant,
             sizes,
             onSale,
             link
